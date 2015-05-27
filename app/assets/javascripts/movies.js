df9053441388026ed5fd8adc86abc09310f2bff5
Laertis Pappas <laertis.pappas@gmail.com>
2015-05-27 03:11:43 +0300

    Added disable input on search

--- app/assets/javascripts/movies.js
+++ app/assets/javascripts/movies.js
@@ -5,6 +5,9 @@ $(document).on('submit','form#form-moviesdb',function(){
     $("span.label").text("Searching");
     $("#movie_title").val();
     $("#movie_description").val();
+
+    $("input").prop('disabled', true);
+
 });
 
 $(document).on("click", "#searched-movie a", function(){
@@ -13,8 +16,10 @@ $(document).on("click", "#searched-movie a", function(){
     $("span.label").removeClass("success");
     $("span.label").addClass("alert");
     $("span.label").text("Getting movie details");
+    $("input").prop('disabled', true);
 
     $.ajax({url: "/search/"+movie_id, success: function(result){
+        $("input").prop('disabled', false);
         $("span.label").addClass("success");
         $("span.label").text("Not Searching");
         $("#searched-movie").empty();
