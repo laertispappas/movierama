3d13977e8fffd0ba4ed9e1cd44cd4a162077f8c6
Laertis Pappas <laertis.pappas@gmail.com>
2015-05-30 02:22:25 +0300

    Force ssl

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require "omniauth-google-oauth2"
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -233,7 +235,6 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  require "omniauth-google-oauth2"
   config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], {}
 
   # ==> Warden configuration
